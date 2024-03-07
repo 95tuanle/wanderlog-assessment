2df46e25f22140207df6b643c7b75413ac458ca9
95tuanle <tlna95@icloud.com>
2024-03-06 20:06:53 -0500

    final run on this side

--- signinsignout/javascript.js
+++ signinsignout/javascript.js
@@ -8,18 +8,18 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,35 +32,35 @@ function readLine() {
  */
 
 function processLogs(logs, maxSpan) {
-    // Write your code here
+  // Write your code here
 
-    //
-    // WARNING: Please do not use GitHub Copilot, ChatGPT, or other AI assistants
-    //          when solving this problem!
-    //
-    // We use these tools in our coding too, but in our interviews, we also don't
-    // allow using these, and want to see how we do without them.
-    //
+  //
+  // WARNING: Please do not use GitHub Copilot, ChatGPT, or other AI assistants
+  //          when solving this problem!
+  //
+  // We use these tools in our coding too, but in our interviews, we also don't
+  // allow using these, and want to see how we do without them.
+  //
 }
 
 
 function main() {
-    const ws = process.stdout;
+  const ws = process.stdout;
 
-    const logsCount = parseInt(readLine().trim(), 10);
+  const logsCount = parseInt(readLine().trim(), 10);
 
-    let logs = [];
+  let logs = [];
 
-    for (let i = 0; i < logsCount; i++) {
-        const logsItem = readLine();
-        logs.push(logsItem);
-    }
+  for (let i = 0; i < logsCount; i++) {
+    const logsItem = readLine();
+    logs.push(logsItem);
+  }
 
-    const maxSpan = parseInt(readLine().trim(), 10);
+  const maxSpan = parseInt(readLine().trim(), 10);
 
-    const result = processLogs(logs, maxSpan);
+  const result = processLogs(logs, maxSpan);
 
-    ws.write(result.join('\n') + '\n');
+  ws.write(result.join('\n') + '\n');
 
-    ws.end();
+  ws.end();
 }
