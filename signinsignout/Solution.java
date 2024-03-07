feab201f0b98fd4095590e90bae9a9e83ff35ce8
95tuanle <tlna95@icloud.com>
2024-03-06 19:32:46 -0500

    wrong

--- signinsignout/Solution.java
+++ signinsignout/Solution.java
@@ -1,13 +1,10 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static java.util.stream.Collectors.toList;
 
 class Result {
@@ -31,6 +28,27 @@ class Result {
         // We use these tools in our coding too, but in our interviews, we also don't
         // allow using these, and want to see how we do without them.
         //
+
+        HashMap<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        System.out.println(logs.size() + " " + maxSpan);
+        for (String log : logs) {
+            String[] logArr = log.split(" ");
+            String userId = logArr[0];
+            String timestamp = logArr[1];
+            String action = logArr[2];
+            if (action.equals("sign-in")) {
+                map.put(userId, Integer.parseInt(timestamp));
+            } else {
+
+                if (map.containsKey(userId) && Integer.parseInt(timestamp) - map.get(userId) <= maxSpan) {
+                    System.out.println(userId + " " + timestamp + " " + map.get(userId) + " " + (Integer.parseInt(timestamp) - map.get(userId)));
+                    result.add(userId);
+                }
+            }
+        }
+        result.sort(Comparator.comparingInt(Integer::parseInt));
+        return result;
     }
 
 }
@@ -48,18 +66,13 @@ public class Solution {
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
-        })
-            .collect(toList());
+        }).collect(toList());
 
         int maxSpan = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<String> result = Result.processLogs(logs, maxSpan);
 
-        bufferedWriter.write(
-            result.stream()
-                .collect(joining("\n"))
-            + "\n"
-        );
+        bufferedWriter.write(String.join("\n", result) + "\n");
 
         bufferedReader.close();
         bufferedWriter.close();
